2cbf10ba4f8600a2d3e3fbf1de4719a9eb4a1fc5
satsukizzz <dn.asp01@gmail.com>
2021-11-16 17:22:20 +0900

    CANVAS sample

--- docs/main.js
+++ docs/main.js
@@ -1,53 +1,67 @@
 'use strict';
 
-function preload() {
-  //背景の画像
-  this.load.spritesheet(
-    'world',
-    'assets/world.png',
-    {frameWidth: 16, frameHeight: 16}
-  );
-  //光
-  this.load.spritesheet(
-    'light',
-    'assets/pipo-hikarimono002.png',
-    { frameWidth: 32, frameHeight: 32 }
-  );
-};
-
-function create() {
-  //背景の描画；
-  for (let y = 0; y < c.VERTICAL_PIXELS; y++) {
-      for (let x = 0; x < c.HORIZONTAL_PIXELS; x++) {
-      this.add.image( c.GRID_SIZE*x + c.GRID_PAD, c.GRID_SIZE*y + c.GRID_PAD, 'world', firstMap[y][x]);
-    }
+class FirstScene extends Phaser.Scene {
+  constructor () {
+    super({key: "firstScene", active: true});
+    this.move = 0;
   }
 
-  let light = this.add.sprite(400, 320, 'light');
+  preload() {
+    //背景の画像
+    this.load.spritesheet(
+      'world',
+      'assets/world.png',
+      {frameWidth: 16, frameHeight: 16}
+    );
+    //光
+    this.load.spritesheet(
+      'light',
+      'assets/pipo-hikarimono002.png',
+      { frameWidth: 32, frameHeight: 32 }
+    );
+  };
 
-  this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('light', { start: 0, end: 2 }),
-      frameRate: 5,
-      repeat: -1
-  });
+  create() {
+    //背景の描画；
+    this.image = Array.from(firstMap);
+    for (let y = 0; y < c.VERTICAL_PIXELS; y++) {
+        for (let x = 0; x < c.HORIZONTAL_PIXELS; x++) {
+        this.image[y][x] = this.add.image( c.GRID_SIZE*x + c.GRID_PAD, c.GRID_SIZE*y + c.GRID_PAD, 'world', firstMap[y][x]);
+      }
+    }
 
-  light.anims.play('down', false);
-};
+    this.text = this.add.text(100, 100, this.move).setFontSize(64).setColor('#ff0');
+    this.text2 = this.add.text(100, 150, this.move).setFontSize(64).setColor('#f00');
+    // this.text.setDepth(1);
+    this.text2.setDepth(3);
+    
+    const light = this.add.sprite(400, 320, 'light');
+    light.setDepth(2);
 
-function update() {
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('light', { start: 0, end: 2 }),
+        frameRate: 5,
+        repeat: -1
+    });
 
-};
+    light.play('down', false);
+  };
+
+  update() {
+    this.text.x = 100 + this.move;
+    this.text.y = 100 + this.move;
+    this.move += 0.1;
+  };
+}
 
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS, //なぜかWEBGLだとdepthがうまくいかずに別の画像レンダーされてしまう問題があるのでCANVAS
   width: c.CANVAS_WIDTH,
   height: c.CANVAS_HEIGHT,
-  scene: {
-    preload,
-    create,
-    update,
-  },
+  scene: [
+    FirstScene,
+  ],
 };
 
 const game = new Phaser.Game(config);
