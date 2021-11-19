31d70fa3dd7a85be91def90d141fab2c59f77c9b
satsukizzz <dn.asp01@gmail.com>
2021-11-19 18:06:11 +0900

    add other 3 pair of objects corresponding to S, A, D

--- docs/main.js
+++ docs/main.js
@@ -87,6 +87,84 @@ function create() {
 
   this.lightTwo.anims.play('light-2', false);
 
+  this.lightspaceOne = this.physics.add.sprite(gc2p(10), gc2p(12), 'lightspace').setScale(0.5);
+  this.lightspaceOne.depth = 1;
+
+  this.anims.create({
+    key: 'lightspace-1',
+    frames: this.anims.generateFrameNumbers('lightspace', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightspaceOne.anims.play('lightspace-1', false);
+  this.lightspaceOne.visible = false;
+
+  this.lightOne = this.physics.add.sprite(gc2p(10), gc2p(10), 'light');
+  this.lightOne.depth = 2;
+
+  this.anims.create({
+    key: 'light-1',
+    frames: this.anims.generateFrameNumbers('light', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightOne.anims.play('light-1', false);
+  this.lightOne.visible = false;
+
+  this.lightspaceThree = this.physics.add.sprite(gc2p(1), gc2p(14), 'lightspace').setScale(0.5);
+  this.lightspaceThree.depth = 1;
+
+  this.anims.create({
+    key: 'lightspace-3',
+    frames: this.anims.generateFrameNumbers('lightspace', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightspaceThree.anims.play('lightspace-3', false);
+  this.lightspaceThree.visible = false;
+
+  this.lightThree = this.physics.add.sprite(gc2p(5), gc2p(14), 'light');
+  this.lightThree.depth = 2;
+
+  this.anims.create({
+    key: 'light-3',
+    frames: this.anims.generateFrameNumbers('light', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightThree.anims.play('light-3', false);
+  this.lightThree.visible = false;
+
+  this.lightspaceFour = this.physics.add.sprite(gc2p(20), gc2p(17), 'lightspace').setScale(0.5);
+  this.lightspaceFour.depth = 1;
+
+  this.anims.create({
+    key: 'lightspace-4',
+    frames: this.anims.generateFrameNumbers('lightspace', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightspaceFour.anims.play('lightspace-4', false);
+  this.lightspaceFour.visible = false;
+
+  this.lightFour = this.physics.add.sprite(gc2p(17), gc2p(17), 'light');
+  this.lightFour.depth = 2;
+
+  this.anims.create({
+    key: 'light-4',
+    frames: this.anims.generateFrameNumbers('light', { start: 6, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.lightFour.anims.play('light-4', false);
+  this.lightFour.visible = false;
+
   // add obstacles
 
   // add text objects
@@ -103,6 +181,9 @@ function create() {
   // add flags
   this.flags = {};
   this.flags.isTwoOverlap = false;
+  this.flags.isOneOverlap = false;
+  this.flags.isThreeOverlap = false;
+  this.flags.isFourOverlap = false;
   this.flags.isCompleted = false;
 
   // collisions and overlaps
@@ -110,6 +191,28 @@ function create() {
   this.physics.add.overlap(this.lightTwo, this.lightspaceTwo,
     (a, b) => {
       this.flags.isTwoOverlap = true;
+      b.destroy();
+    },
+    null, this);
+  this.physics.add.collider(this.playerLight, this.lightOne);
+  this.physics.add.overlap(this.lightOne, this.lightspaceOne,
+    (a, b) => {
+      this.flags.isOneOverlap = true;
+      b.destroy();
+    },
+    null, this);
+  this.physics.add.collider(this.playerLight, this.lightThree);
+  this.physics.add.overlap(this.lightThree, this.lightspaceThree,
+    (a, b) => {
+      this.flags.isThreeOverlap = true;
+      b.destroy();
+    },
+    null, this);
+  this.physics.add.collider(this.playerLight, this.lightFour);
+  this.physics.add.overlap(this.lightFour, this.lightspaceFour,
+    (a, b) => {
+      this.flags.isFourOverlap = true;
+      b.destroy();
     },
     null, this);
 };
@@ -119,6 +222,12 @@ function update() {
   this.playerLight.setVelocityY(0);
   this.lightTwo.setVelocityX(0);
   this.lightTwo.setVelocityY(0);
+  this.lightOne.setVelocityX(0);
+  this.lightOne.setVelocityY(0);
+  this.lightThree.setVelocityX(0);
+  this.lightThree.setVelocityY(0);
+  this.lightFour.setVelocityX(0);
+  this.lightFour.setVelocityY(0);
 
   // キー入力
   if (this.keys.keyW.isDown) {
@@ -151,10 +260,22 @@ function update() {
   }
 
   // flag判定の伝播
-  this.flags.isCompleted = this.flags.isTwoOverlap;
+  this.flags.isCompleted = this.flags.isFourOverlap;
 
   // flagによるオブジェクトの描画
   this.completedText.visible = this.flags.isCompleted;
+  if (this.flags.isTwoOverlap) {
+    this.lightOne.visible = true;
+    this.lightspaceOne.visible = true;
+  }
+  if (this.flags.isOneOverlap) {
+    this.lightThree.visible = true;
+    this.lightspaceThree.visible = true;
+  }
+  if (this.flags.isThreeOverlap) {
+    this.lightFour.visible = true;
+    this.lightspaceFour.visible = true;
+  }
 
   // flagのオフ
   this.flags.isTwoOverlap = false;
